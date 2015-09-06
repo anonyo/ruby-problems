a93f2cb36ac7512dec1c36e9acc6fe2db5d6a280
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-05 21:04:39 -0400

    Fix spec expect method

--- spec/lib/blackjack/deal_spec.rb
+++ spec/lib/blackjack/deal_spec.rb
@@ -2,12 +2,9 @@ require 'spec_helper'
 
 describe Deal do
   describe '#twenty_one?' do
-   it { is_expected(Deal.twenty_one?(3, 4, 5, 6, 3)).to  be_true }
-   it { is_expected(Deal.twenty_one?(3, 11, 10)).to  be_false }
-   it { is_expected(Deal.twenty_one?(10, 11)).to  be_true }
-   it { is_expected(Deal.twenty_one?(10, 10)).to  be_true }
+   it { expect(Deal.twenty_one?(3, 4, 5, 6, 3)).to  be_true }
+   it { expect(Deal.twenty_one?(3, 11, 10)).to  be_false }
+   it { expect(Deal.twenty_one?(10, 11)).to  be_true }
+   it { expect(Deal.twenty_one?(10, 10)).to  be_false }
   end
-end
-
-class Deal
 end
