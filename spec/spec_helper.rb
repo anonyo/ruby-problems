f13ad0fd514616e87737d78355f5252e9e4c5e55
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-05 21:00:03 -0400

    Require all folders under lib on spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ FactoryGirl.find_definitions
 
 PROJECT_ROOT = File.expand_path("../..", __FILE__)
 
-Dir.glob(File.join(PROJECT_ROOT, "lib", "*.rb")).each do |file|
+Dir.glob(File.join(PROJECT_ROOT, "lib", "**", "*.rb")).each do |file|
   require file
 end
 
